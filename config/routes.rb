bd451fb37ecb71c2b98cca033ba0ea58e52907d3
cwmacken <cwmacken@gmail.com>
2013-11-11 19:21:50 -0800

    issues controller routes and pages created; no functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Hoptoit::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#index'
 
+  get 'issues' => 'issues#index'
+
+  get 'issues/new' => 'issues#new'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
