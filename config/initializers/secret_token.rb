a1e1b82114bb4f31e7e5d4f7ade2011f7bf36661
Evan Pavlica <evan@xicreative.net>
2013-11-11 16:31:05 -0800

    Initial Commit -- new rails project w/ postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hoptoit::Application.config.secret_key_base = 'f5232e46d1de371b6d3892847cd9d72bba509ee545dc935f8823fd86d9127b5d3e5da863f3b4a83c34d04c6e262aaa7b2e7ed9cb2e3d4575b4c89f4246742847'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hoptoit::Application.config.session_store :cookie_store, key: '_hoptoit_session'
