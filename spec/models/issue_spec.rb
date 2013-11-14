098cea2f6251d84a2f53eba80f146b45b6499f29
cwmacken <cwmacken@gmail.com>
2013-11-13 21:42:41 -0800

    2 tests addded

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
-describe Issue  do
-  it 'is invalid wihtout a title ' do 
+describe Issue do
+
+  it 'is invalid without a title ' do 
     expect(Issue.create()).to be_invalid
-  end  
+  end 	
+
+  it 'is valid with title =' do
+  	expect(Issue.create(:title => 'test title')).to be_valid
+  end
 end
