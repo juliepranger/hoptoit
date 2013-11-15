e82ca672c1c88c483e823f0c57a77bb370ff643c
Julie Pranger <juliepranger@gmail.com>
2013-11-14 18:29:12 -0800

    Action Mailer working with Letter Opener, Gmail capability is commented out for now

---
+++ app/mailers/users_mailer.rb
@@ -0,0 +1,14 @@
+class UsersMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.new_user_notification.subject
+  #
+  def new_user_notification(user)
+    @user = user
+
+    mail to: @user.email, subject: "Thanks for joining Hop To It!"
+  end
+end
