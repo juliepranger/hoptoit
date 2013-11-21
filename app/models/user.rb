dd4e666a96c853bbe6d23926521b4ed607a93f0d
Julie Pranger <juliepranger@gmail.com>
2013-11-21 12:35:52 -0800

    forgot password working

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,19 @@ class User < ActiveRecord::Base
     BCrypt::Engine.hash_secret(password, self.salt)
   end
 
+    def generate_token(column)
+      begin
+        self[column] = SecureRandom.urlsafe_base64
+      end while User.exists?(column => self[column])
+  end
+
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UsersMailer.password_reset(self).deliver
+  end
+
   private
 
   def hash_password
@@ -32,13 +45,6 @@ class User < ActiveRecord::Base
       BCrypt::Engine.hash_secret(password, self.salt)
       password = password_confirmation = nil
     end
-  end
-
-  def send_password_reset
-  	generate_token(:password_reset_token)
-  	self.password_reset_sent_at = Time.zone.now
-  	save!
-  	UsersMailer.password_reset(self).deliver
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131121174905) do
+ActiveRecord::Schema.define(version: 20131121202550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20131121174905) do
     t.datetime "updated_at"
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
+    t.string   "password_digest"
+    t.string   "auth_token"
   end
 
 end
