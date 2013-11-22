a1f1fedcd6f2c5aaa9eadcf81a8bc1d53cfd3276
Julie Pranger <juliepranger@gmail.com>
2013-11-22 15:58:04 -0800

    token generator test passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
   before do
-    @testuser = User.create(first_name: 'Julie', last_name: 'Pranger', email: 'juliepranger@gmail.com', address: '1520 2nd St.', city: 'Santa Monica', state: 'CA', zipcode: '90405', password: "julie")
+    @testuser = User.create(first_name: 'Julie', last_name: 'Pranger', email: 'juliepranger@gmail.com', address: '1520 2nd St.', city: 'Santa Monica', state: 'CA', zipcode: '90405', password: "julie", password_confirmation: "julie")
     @adminuser = User.create(first_name: 'Adam', last_name: 'Bouck', email: 'adam.j.bouck@gmail.com', address: '1520 2nd St.', city: 'Santa Monica', state: 'CA', zipcode: '90405', admin: "true")
   end
 
@@ -35,10 +35,20 @@ describe User do
   		expect(FactoryGirl.create(:user)).to be_valid
   	end
 
-  # context 'when user logs in'
-  #   describe "when password matches confirmation"
-  #     it 'signs in' do
-        
-        
+  describe 'when password is authenticated' do
+      it 'signs in' do
+        expect(@testuser.authenticate("julie")).to eq(true)
+        end
+
+      it 'does not allow user to sign in' do
+        expect(@testuser.authenticate("adam")).to eq(false)
+      end
+    end
+
+  describe 'when token is generated' do
+    it 'generates token' do
+      @testuser.generate_token(:password_reset_token)
+      expect(@testuser.password_reset_token).to_not eq(nil)
+    end
   end
 end
