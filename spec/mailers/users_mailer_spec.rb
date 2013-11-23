52f5f09bf13af4f0d2537e9dbd78b325986548ff
Julie Pranger <juliepranger@gmail.com>
2013-11-22 17:11:03 -0800

    users mailer password reset tests pass

--- spec/mailers/users_mailer_spec.rb
+++ spec/mailers/users_mailer_spec.rb
@@ -12,4 +12,18 @@ describe UsersMailer do
     end
   end
 
+  describe "password_reset" do
+    let(:user) { FactoryGirl.create(:user, :password_reset_token => "anything") }
+    let(:mail) { UsersMailer.password_reset(user) }
+
+    it "sends user password reset url" do
+      mail.subject.should eq("Reset Your Password")
+      mail.to.should eq([user.email])
+      mail.from.should eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      mail.body.encoded.should match(edit_password_reset_path(user.password_reset_token))
+    end
+  end
 end
