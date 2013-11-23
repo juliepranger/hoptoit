a5f3477f9ebc9332bd01d450ff9970cef95f0f00
Evan Pavlica <evan@xicreative.net>
2013-11-22 17:28:29 -0800

    fixed some testing issues

--- spec/factories/charity_verifiers.rb
+++ spec/factories/charity_verifiers.rb
@@ -2,6 +2,22 @@
 
 FactoryGirl.define do
   factory :charity_verifier do
-    org_name "MyString"
+    ein 123456789
+    org_name "Test name"
+    street_address "Test Addy"
+    city "Test City"
+    state "Test State"
+    zipcode "test zipcode"
+    in_care_of_name "test person"
+    subsection_code 123
+    classification_code 123
+    affiliation_code 123
+    deductability_code 123
+    foundation_code 123
+    eo_status_code 123 
+    income_code 123 
+    tax_period 123 
+    secondary_name "test other name"
+    ntee_code "test code"
   end
 end
